0aeb4b7d7771dca9a770864becb1eb8ff26374ee
Kristine Larson <kristinem.larson@gmail.com>
2020-09-04 15:41:07 -0600

    more

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["numpy","wget","scipy","matplotlib","requests"]
 
 setup(
     name="gnssrefl0",
-    version="0.0.6",
+    version="0.0.7",
     author="Kristine Larson",
     author_email="kristinem.larson@gmail.com",
     description="A package to do not much ",
