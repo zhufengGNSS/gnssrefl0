36f485c5b5ed1590a4a52f49839af2b6a19f898e
Kristine Larson <kristinem.larson@gmail.com>
2020-09-04 17:30:59 -0600

    more

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     entry_points ={ 
         'console_scripts': [ 
             'gnssir = gnssrefl0.gnssir_cl:main',
-            'rinex2snr = gnssrefl0.rinex2snr:main',
+            'rinex2snr = gnssrefl0.rinex2snr_cl:main',
             'download_rinex = gnssrefl0.download_rinex:main',
             'make_json_input = gnssrefl0.make_json_input:main',
             ], 
